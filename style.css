c92d7ec5d11466c0933f70c0ff85cdd722aa5b04
d-3488D <75127521+d-3488D@users.noreply.github.com>
2021-05-20 20:06:10 +0300

    ad

---
+++ style.css
@@ -0,0 +1,198 @@
+body {
+    font-family: sans-serif;
+    background-color: black;
+}
+
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    margin-bottom: 20px;
+}
+
+.button {
+    background-color: #36bbce;
+    color: black;
+    text-decoration: none;
+    padding: 15px;
+    font-size: 24px;
+    border-radius: 10px;
+    display: block;
+}
+
+nav {
+    border-top: 5px solid white;
+    border-bottom: 5px solid white;
+    padding: 25px 100px 25px 95px;
+    display: flex;
+    justify-content: space-between;
+}
+
+.nav-link {
+    font-size: 24px;
+    color: white;
+    text-decoration: none;
+    display: block;
+    background-color: #87CEEB;
+    border-radius: 10px;
+    padding: 15px
+    
+    
+    
+}
+
+h1 {
+    text-align: center;
+    color: white;
+    font-size: 36px;
+    margin-top: 45px;
+    margin-bottom: 40px;
+}
+
+.post-text {
+    font-size: 24px;
+}
+
+.info {
+    display: flex;
+    flex-wrap: wrap;
+    max-width: 1100px;
+    margin: 0 auto;
+    position: relative; 
+
+}
+
+.post {
+    background-color: white;
+    padding: 15px 45px 30px 45px;
+    width: 400px;
+    margin: 20px;
+    position: relative;
+}
+
+.article-but {
+    text-decoration: none;
+    background-color: #133aac;
+    color: white;
+    font-size: 24px;
+    text-align: center;
+    padding: 10px 75px 10px 75px;
+    display: block;
+    positon: absolute;
+    top: 0px;
+    right: 0px;
+    
+
+}
+.article-but:hover {
+    text-decoration: none;
+    background-color: orange;
+    color: white;
+    font-size: 24px;
+    text-align: center;
+    padding: 10px 75px 10px 75px;
+    display: block;
+    positon: absolute;
+    top: 0px;
+    right: 0px;
+    
+
+}
+
+.full-image {
+    margin: 100px 0px 100px 0px;
+}
+footer {
+    background-color: white;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 30px 30px 20px 45px;
+
+}
+
+.footer-text {
+    font-size: 24px;
+    margin-right: 30px;
+}
+
+.footer-text span {
+    color: #850AE5;
+    font-weight: bolder;
+}
+.block {
+    position: fixed;
+    right: 0px; 
+    top: 230px;
+    background-color: #1E90FF;
+    width: 80px;
+    padding: 30px;
+    border-radius: 20px;
+}
+.new {
+   position: absolute;
+   top: 0px;
+  width: 100px;
+  height: 100px;
+  right: 450px;
+  border-radius: 40px
+}
+.article-but:focus {
+    text-decoration: none;
+    background-color: green;
+    color: white;
+    font-size: 24px;
+    text-align: center;
+    padding: 10px 75px 10px 75px;
+    display: block;
+    positon: absolute;
+    top: 0px;
+    right: 0px;
+}
+.button:hover {
+    background-color: #FFFF00;
+    color: black;
+    text-decoration: none;
+    padding: 15px;
+    font-size: 24px;
+    border-radius: 10px;
+    display: block;
+}
+.button:focus {
+    background-color:orange;
+    color: black;
+    text-decoration: none;
+    padding: 15px;
+    font-size: 24px;
+    border-radius: 10px;
+    display: block;
+}
+.nav-link:hover {
+    font-size: 24px;
+    color: #00008b;
+    text-decoration: none;
+    display: block;
+}
+
+ .nav-link:focus {
+ font-size: 24px;
+ color:purple ;
+ text-decoration: none;
+ display: block;
+ }
+ h1:hover {
+     background-color:#4169E1; 
+
+ }
+ .hu:hover {
+  background-color: #E0FFFF;
+ }
+
+.post-img {
+    opacity: 0.5;
+
+}
+.post-img:hover {
+    opacity: 1;
+}
